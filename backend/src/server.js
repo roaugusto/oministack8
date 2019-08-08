d1b39d708ea25378f966a51f35eea41205bfaa07
Rodrigo Augusto <rodrigo.augusto@digitalpages.com.br>
2019-08-08 10:45:22 -0300

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0-ihwtq.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true
+})
+
+server.use(express.json());
+server.use(routes);
+server.listen(3333);
